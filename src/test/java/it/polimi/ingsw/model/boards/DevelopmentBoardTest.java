d666c285106d96bde2b8131b5968fdc947fcd080
aralara <lara.ferro@mail.polimi.it>
2021-04-05 23:49:53 +0200

    Added test classes
    Added LeaderBoard and DevelopmentBoard test classes

---
+++ src/test/java/it/polimi/ingsw/model/boards/DevelopmentBoardTest.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.model.boards;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DevelopmentBoardTest {
+
+    @Test
+    public void testGetAllCards() {
+    }
+
+    @Test
+    public void testGetActiveProductions() {
+    }
+
+    @Test
+    public void testAddDevCard() {
+    }
+
+    @Test
+    public void testCheckDevCardAddable() {
+    }
+}
