ad0435fdf23a14614548e58056d9c4cc52862ea6
aralara <lara.ferro@mail.polimi.it>
2021-04-06 22:08:27 +0200

    Implemented factory classes
    Implemented methods of LorenzoDevFactory and LorenzoFaithFactory classes

--- src/main/java/it/polimi/ingsw/model/cards/factory/LorenzoDevFactory.java
+++ src/main/java/it/polimi/ingsw/model/cards/factory/LorenzoDevFactory.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.model.cards.factory;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import it.polimi.ingsw.model.cards.card.LorenzoDev;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LorenzoDevFactory implements LorenzoCardFactory {
@@ -13,6 +18,20 @@ public class LorenzoDevFactory implements LorenzoCardFactory {
 
     @Override
     public List<LorenzoDev> loadCardFromFile(String fileName) {
-        return null;
+        Gson gson = new Gson();
+        List<LorenzoDev> lorenzoDevDeck = new ArrayList<>();
+
+        try {
+            LorenzoDev[] jsonLorenzoDevs;
+            JsonReader reader = new JsonReader(new FileReader(fileName));
+            jsonLorenzoDevs = gson.fromJson(reader, LorenzoDev[].class);
+
+            for (LorenzoDev lorenzoDev : jsonLorenzoDevs) {
+                lorenzoDevDeck.add(new LorenzoDev(lorenzoDev.getColor(), lorenzoDev.getQuantity()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lorenzoDevDeck;
     }
 }
--- src/main/java/it/polimi/ingsw/model/cards/factory/LorenzoFaithFactory.java
+++ src/main/java/it/polimi/ingsw/model/cards/factory/LorenzoFaithFactory.java
@@ -1,7 +1,13 @@
 package it.polimi.ingsw.model.cards.factory;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import it.polimi.ingsw.model.cards.card.LorenzoDev;
 import it.polimi.ingsw.model.cards.card.LorenzoFaith;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LorenzoFaithFactory implements LorenzoCardFactory {
@@ -13,6 +19,20 @@ public class LorenzoFaithFactory implements LorenzoCardFactory {
 
     @Override
     public List<LorenzoFaith> loadCardFromFile(String fileName) {
-        return null;
+        Gson gson = new Gson();
+        List<LorenzoFaith> lorenzoFaithDeck = new ArrayList<>();
+
+        try {
+            LorenzoFaith[] jsonLorenzoFaiths;
+            JsonReader reader = new JsonReader(new FileReader(fileName));
+            jsonLorenzoFaiths = gson.fromJson(reader, LorenzoFaith[].class);
+
+            for (LorenzoFaith lorenzoFaith : jsonLorenzoFaiths) {
+                lorenzoFaithDeck.add(new LorenzoFaith(lorenzoFaith.isRefresh(), lorenzoFaith.getAmount()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lorenzoFaithDeck;
     }
 }
