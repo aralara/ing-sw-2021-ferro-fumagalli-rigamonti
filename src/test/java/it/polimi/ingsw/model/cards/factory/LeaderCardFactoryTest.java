5a4a66374f2a32e9c1eccabe9d92f98877900958
davide-rigamonti-polimi <davide2.rigamonti@mail.polimi.it>
2021-04-07 17:37:28 +0200

    Implemented Factory class for LeaderCard, updated Leader_Card_File.json and updated pom.xml file
    Implemented loadCardFromFile method in LeaderCardFactory and created relative Test class, added dependency for gson-extras in pom.xml and updated attribute names in Leader_Card_File.json

---
+++ src/test/java/it/polimi/ingsw/model/cards/factory/LeaderCardFactoryTest.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model.cards.factory;
+
+import it.polimi.ingsw.model.FileNames;
+import it.polimi.ingsw.model.cards.card.LeaderCard;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LeaderCardFactoryTest {
+
+    @Test
+    public void loadCardFromFile() {
+    }
+}
