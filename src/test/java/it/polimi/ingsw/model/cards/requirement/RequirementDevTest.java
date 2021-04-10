f674dc5e350e0d7e43b4cdcb505d9bff574d211a
stefuma19 <stefano14.fumagalli@mail.polimi.it>
2021-04-10 23:07:01 +0200

    Added tests for Requirement classes
    Added test methods for classes in Requirement package

---
+++ src/test/java/it/polimi/ingsw/model/cards/requirement/RequirementDevTest.java
@@ -0,0 +1,71 @@
+package it.polimi.ingsw.model.cards.requirement;
+
+import it.polimi.ingsw.model.boards.PlayerBoard;
+import it.polimi.ingsw.model.cards.card.CardColors;
+import it.polimi.ingsw.model.cards.card.DevelopmentCard;
+import it.polimi.ingsw.model.games.MultiGame;
+import it.polimi.ingsw.model.storage.Production;
+import it.polimi.ingsw.model.storage.Resource;
+import it.polimi.ingsw.model.storage.ResourceType;
+import it.polimi.ingsw.model.storage.Shelf;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class RequirementDevTest {
+
+    @Test
+    public void testGetColor() {
+        RequirementDev rd = new RequirementDev(CardColors.YELLOW, 2,1);
+        assertEquals(CardColors.YELLOW,rd.getColor());
+    }
+
+    @Test
+    public void testGetLevel() {
+        RequirementDev rd = new RequirementDev(CardColors.YELLOW, 2,1);
+        assertEquals(2,rd.getLevel());
+    }
+
+    @Test
+    public void testGetNumber() {
+        RequirementDev rd = new RequirementDev(CardColors.YELLOW, 2,1);
+        assertEquals(1,rd.getNumber());
+    }
+
+    @Test
+    public void testCheckRequirement() {
+        PlayerBoard pb = new PlayerBoard(new MultiGame("Bonucci"),"Bonucci");
+
+        pb.getDevelopmentBoard().addDevCard(new DevelopmentCard(2,CardColors.YELLOW,1,
+                new Production(new ArrayList<>(List.of(new Resource(ResourceType.SERVANT,2))),
+                        new ArrayList<>(List.of(new Resource(ResourceType.FAITH,2)))),
+                        new ArrayList<>(List.of(new Resource(ResourceType.SHIELD,2)))),1);
+        pb.getDevelopmentBoard().addDevCard(new DevelopmentCard(2,CardColors.BLUE,1,
+                new Production(new ArrayList<>(List.of(new Resource(ResourceType.SERVANT,2))),
+                        new ArrayList<>(List.of(new Resource(ResourceType.FAITH,2)))),
+                new ArrayList<>(List.of(new Resource(ResourceType.SHIELD,2)))),2);
+        pb.getDevelopmentBoard().addDevCard(new DevelopmentCard(2,CardColors.YELLOW,2,
+                new Production(new ArrayList<>(List.of(new Resource(ResourceType.SERVANT,2))),
+                        new ArrayList<>(List.of(new Resource(ResourceType.FAITH,2)))),
+                new ArrayList<>(List.of(new Resource(ResourceType.SHIELD,2)))),1);
+
+
+        RequirementDev rr = new RequirementDev(CardColors.YELLOW, 2,1);
+        assertTrue(rr.checkRequirement(pb));
+
+        rr = new RequirementDev(CardColors.YELLOW, 1,2);
+        assertTrue(rr.checkRequirement(pb));
+
+        rr = new RequirementDev(CardColors.PURPLE, 1,1);
+        assertFalse(rr.checkRequirement(pb));
+
+        rr = new RequirementDev(CardColors.BLUE, 1,1);
+        assertTrue(rr.checkRequirement(pb));
+
+        rr = new RequirementDev(CardColors.BLUE, 2,1);
+        assertFalse(rr.checkRequirement(pb));
+    }
+}
--- src/test/java/it/polimi/ingsw/model/cards/requirement/RequirementResTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/requirement/RequirementResTest.java
@@ -16,10 +16,28 @@ import static org.junit.Assert.*;
 public class RequirementResTest {
 
     @Test
-    public void getResource() {
+    public void testGetResource() {
+        RequirementRes rr = new RequirementRes(new Resource(ResourceType.SHIELD,4));
+
+        assertEquals(4,rr.getResource().getQuantity());
+        assertEquals(ResourceType.SHIELD,rr.getResource().getResourceType());
     }
 
     @Test
-    public void checkRequirement() {
+    public void testCheckRequirement() {
+        PlayerBoard pb = new PlayerBoard(new MultiGame("Bonucci"),"Bonucci");
+        Shelf s1 = new Shelf(ResourceType.SHIELD,new Resource(ResourceType.SHIELD,3),3,false );
+        Shelf s2 = new Shelf(ResourceType.SHIELD,new Resource(ResourceType.SHIELD,2),2,true );
+
+        pb.getWarehouse().changeConfiguration(new ArrayList<>(List.of(s1,s2)));
+
+        RequirementRes rr = new RequirementRes(new Resource(ResourceType.SHIELD,4));
+        assertTrue(rr.checkRequirement(pb));
+
+        rr = new RequirementRes(new Resource(ResourceType.COIN,4));
+        assertFalse(rr.checkRequirement(pb));
+
+        rr = new RequirementRes(new Resource(ResourceType.SHIELD,6));
+        assertFalse(rr.checkRequirement(pb));
     }
 }
