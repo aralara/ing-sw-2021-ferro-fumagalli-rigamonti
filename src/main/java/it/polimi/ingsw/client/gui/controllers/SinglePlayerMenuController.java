cbd6deec4dadcf3e3da622a16916eb4ef090347f
aralara <lara.ferro@mail.polimi.it>
2021-05-19 00:52:31 +0200

    Added panel controllers and graphical resources

---
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/SinglePlayerMenuController.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.client.gui.controllers;
+
+import javafx.event.ActionEvent;
+
+public class SinglePlayerMenuController {
+    public void startOnlineGame(ActionEvent actionEvent) {
+    }
+
+    public void startOfflineGame(ActionEvent actionEvent) {
+    }
+}
